d6fb914549bb86423025bccdcf7f3bcc55ef608d
Swany710 <eswanberg0@gmail.com>
2025-12-26 16:38:19 -0600

    Add isEmailProvider function to validate email providers

--- src/integrations/email/email.types.ts
+++ src/integrations/email/email.types.ts
@@ -1,3 +1,7 @@
 export type EmailProvider = 'gmail' | 'outlook';
 
 export const emailProviders: EmailProvider[] = ['gmail', 'outlook'];
+
+export function isEmailProvider(value: any): value is EmailProvider {
+  return typeof value === 'string' && (value === 'outlook' || value === 'gmail');
+}
