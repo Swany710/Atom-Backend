83f8a163b1dc6d5f0ad6d1bf4ec63ef5fe0e3a8c
Swany710 <eswanberg0@gmail.com>
2025-12-26 20:44:34 -0600

    Refactor EmailModule providers and exports

--- src/integrations/email/email.module.ts
+++ src/integrations/email/email.module.ts
@@ -1,4 +1,4 @@
-limport { Module, Provider } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
@@ -10,6 +10,7 @@ import { EmailOAuthController } from './email-oauth.controller';
 import { EmailController } from './email.controller';
 
 import { EmailConnection } from './email-connection.entity';
+import { emailProviderFactory, EMAIL_PROVIDER } from './email.provider';
 
 export const EMAIL_PROVIDER = 'EMAIL_PROVIDER';
 
@@ -29,8 +30,8 @@ const emailProviderFactory: Provider = {
 
 @Module({
   imports: [TypeOrmModule.forFeature([EmailConnection])],
-  providers: [EmailService, OutlookEmailService, GmailService, emailProviderFactory, EmailOAuthService],
+  providers: [OutlookEmailService, GmailService, emailProviderFactory, EmailOAuthService],
   controllers: [EmailOAuthController, EmailController],
-  exports: [OutlookEmailService, EMAIL_PROVIDER],
+  exports: [EMAIL_PROVIDER],
 })
 export class EmailModule {}
